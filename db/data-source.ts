9d10da45af6a612861dd0707f5a5c208b4a6cd7a
Tyrone Amorim <tyrone@trustdeveloper.com.br>
2025-03-04 02:42:27 -0300

    fix: environment validation

--- db/data-source.ts
+++ db/data-source.ts
@@ -1,7 +1,9 @@
 import { DataSource } from 'typeorm';
 import * as dotenv from 'dotenv';
 
-dotenv.config();
+if (process.env.NODE_ENV !== 'production') {
+  dotenv.config();
+}
 
 export const AppDataSource = new DataSource({
   type: 'mariadb',
