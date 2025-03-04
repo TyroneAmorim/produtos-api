4726f6b846d8914a3c6ee190281ffe4679dd0c75
Tyrone Amorim <tyrone@trustdeveloper.com.br>
2025-03-03 23:34:37 -0300

    feat: dto swagger description added

--- src/dto/create-user.ts
+++ src/dto/create-user.ts
@@ -1,14 +1,27 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateUserDto {
+  @ApiProperty({
+    description: 'Nome',
+    example: 'Fulano de Tal',
+  })
   @IsString()
   @IsNotEmpty()
   nome: string;
 
+  @ApiProperty({
+    description: 'Email',
+    example: 'teste@teste.com',
+  })
   @IsString()
   @IsNotEmpty()
   email: string;
 
+  @ApiProperty({
+    description: 'Senha de acesso',
+    example: 'SenhaSegura#$18',
+  })
   @IsString()
   @IsNotEmpty()
   senha: string;
