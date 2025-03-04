a2b08abfaae51162f7127d912fd2bcaa483748fa
Tyrone Amorim <tyrone@trustdeveloper.com.br>
2025-03-03 23:34:08 -0300

    feat: controller swagger descriptions added

--- src/controllers/produtos.controller.ts
+++ src/controllers/produtos.controller.ts
@@ -13,23 +13,28 @@ import { ProdutosService } from 'src/services/produtos.service';
 import { CreateProdutoDto } from '../dto/create-produto';
 import { UpdateProdutoDto } from '../dto/update-produto';
 import { JwtAuthGuard } from 'src/modules/auth/jwt-auth.guard';
+import { ApiBearerAuth, ApiOperation } from '@nestjs/swagger';
 
+@ApiBearerAuth()
 @Controller('produtos')
 export class ProdutosController {
   constructor(private readonly produtosService: ProdutosService) {}
   @UseGuards(JwtAuthGuard)
+  @ApiOperation({ summary: 'Cria um novo produto' })
   @Post()
   async create(@Body() createProdutoDto: CreateProdutoDto) {
     return this.produtosService.create(createProdutoDto);
   }
 
   @UseGuards(JwtAuthGuard)
+  @ApiOperation({ summary: 'Retorna uma lista de todos os produto' })
   @Get()
   async findAll() {
     return this.produtosService.findAll();
   }
 
   @UseGuards(JwtAuthGuard)
+  @ApiOperation({ summary: 'Retorna um produto pelo código' })
   @Get(':codigo')
   async findOne(@Param('codigo') codigo: string) {
     const produto = await this.produtosService.findOne(codigo);
@@ -38,6 +43,7 @@ export class ProdutosController {
   }
 
   @UseGuards(JwtAuthGuard)
+  @ApiOperation({ summary: 'Atualiza um produto' })
   @Put(':codigo')
   async update(
     @Param('codigo') codigo: string,
@@ -49,6 +55,7 @@ export class ProdutosController {
   }
 
   @UseGuards(JwtAuthGuard)
+  @ApiOperation({ summary: 'Exclui um produto pelo código' })
   @Delete(':codigo')
   async remove(@Param('codigo') codigo: string) {
     const deleted = await this.produtosService.remove(codigo);
--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -7,21 +7,25 @@ import {
   Put,
   UseGuards,
 } from '@nestjs/common';
+import { ApiBearerAuth, ApiOperation } from '@nestjs/swagger';
 import { CreateUserDto } from 'src/dto/create-user';
 import { UpdateUserDto } from 'src/dto/update-user';
 import { JwtAuthGuard } from 'src/modules/auth/jwt-auth.guard';
 import { UsersService } from 'src/services/users.service';
 
+@ApiBearerAuth()
 @Controller('users')
 export class UsersController {
   constructor(private readonly userService: UsersService) {}
 
+  @ApiOperation({ summary: 'Cria um novo usuário' })
   @Post()
   async create(@Body() createUserDto: CreateUserDto) {
     return this.userService.create(createUserDto);
   }
 
   @UseGuards(JwtAuthGuard)
+  @ApiOperation({ summary: 'Atualiza os dados de cadastro de um usuário' })
   @Put(':codigo')
   async update(
     @Param('codigo') codigo: string,
