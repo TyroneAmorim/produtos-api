c26d45ad245ada2dd188cf77fd87970aa32e487b
Tyrone Amorim <tyrone@trustdeveloper.com.br>
2025-03-04 02:40:51 -0300

    fix: dtos updated

--- src/dto/update-produto.ts
+++ src/dto/update-produto.ts
@@ -1,3 +1,52 @@
-import { CreateProdutoDto } from './create-produto';
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsString,
+  IsNotEmpty,
+  IsOptional,
+  IsNumber,
+  IsPositive,
+  MaxLength,
+  IsEAN,
+  IsNumberString,
+  Min,
+} from 'class-validator';
 
-export class UpdateProdutoDto extends CreateProdutoDto {}
+export class UpdateProdutoDto {
+  @ApiProperty({
+    description: 'Nome do produto',
+    example: 'Produto 1',
+  })
+  @IsString()
+  @IsNotEmpty()
+  @IsOptional()
+  nome: string;
+
+  @ApiProperty({
+    description: 'Preco',
+    example: '100.50',
+  })
+  @IsNumber()
+  @IsPositive()
+  @IsOptional()
+  preco: number;
+
+  @ApiProperty({
+    description: 'Código de barras',
+    example: '1234567890123',
+  })
+  @MaxLength(13)
+  @IsEAN({ message: 'O código de barras precisa ser válido' })
+  @IsNumberString()
+  @IsOptional()
+  codigo_barras: string;
+
+  @ApiProperty({
+    description: 'Quantidade',
+    example: '1',
+  })
+  @IsPositive()
+  @IsNumber()
+  @Min(1)
+  @IsOptional()
+  quantidade: number;
+}
--- src/dto/update-user.ts
+++ src/dto/update-user.ts
@@ -1,3 +1,52 @@
-import { CreateUserDto } from './create-user';
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsString,
+  IsNotEmpty,
+  MinLength,
+  IsEmail,
+  IsStrongPassword,
+  IsOptional,
+} from 'class-validator';
 
-export class UpdateUserDto extends CreateUserDto {}
+export class UpdateUserDto {
+  @ApiProperty({
+    description: 'Nome',
+    example: 'Fulano de Tal',
+  })
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
+  @IsOptional()
+  nome: string;
+
+  @ApiProperty({
+    description: 'Email',
+    example: 'teste@teste.com',
+  })
+  @IsString()
+  @IsNotEmpty()
+  @IsEmail()
+  @IsOptional()
+  email: string;
+
+  @ApiProperty({
+    description: 'Senha de acesso',
+    example: 'SenhaSegura#$18',
+  })
+  @IsString()
+  @IsNotEmpty()
+  @IsOptional()
+  @IsStrongPassword(
+    {
+      minLength: 8,
+      minUppercase: 2,
+      minSymbols: 1,
+      minNumbers: 2,
+    },
+    {
+      message:
+        'O campo de senha precisa ter: 2 letras maiúsculas, 2 números, 2 símbolos e no mínimo 8 caracteres',
+    },
+  )
+  senha: string;
+}
